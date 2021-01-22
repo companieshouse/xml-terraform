20e67c2fe9c2e504c910aee1da001083998a7287
Marty Fox <martin.fox@kainos.com>
2021-01-22 11:55:15 +0000

    Adding new shared sec group to RDS

--- groups/xml-infrastructure/rds.tf
+++ groups/xml-infrastructure/rds.tf
@@ -62,7 +62,10 @@ module "xml_rds" {
   monitoring_role_arn = data.aws_iam_role.rds_enhanced_monitoring.arn
 
   # RDS Security Group
-  vpc_security_group_ids = [module.xml_rds_security_group.this_security_group_id]
+  vpc_security_group_ids = [
+    module.xml_rds_security_group.this_security_group_id,
+    data.aws_security_group.rds_shared.id
+  ]
 
   # DB subnet group
   subnet_ids = data.aws_subnet_ids.data.ids
