d767c11987081df60d14131c691ec274b9fefe17
nghessen-ch <nghessen@companieshouse.gov.uk>
2026-01-13 15:14:51 +0000

    downgrading oracledb_cloudwatch_alarms version

--- groups/xml-infrastructure/rds.tf
+++ groups/xml-infrastructure/rds.tf
@@ -203,7 +203,7 @@ module "rds_start_stop_schedule" {
 }
 
 module "rds_cloudwatch_alarms" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/oracledb_cloudwatch_alarms?ref=tags/1.0.363"
+  source = "git@github.com:companieshouse/terraform-modules//aws/oracledb_cloudwatch_alarms?ref=tags/1.0.195"
 
   db_instance_id        = module.xml_rds.db_instance_identifier
   db_instance_shortname = upper(var.application)
